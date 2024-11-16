853a7c40f1ccebe7125f72ea5e20778c0898afb1
AdamLevin7 <adamarilevin@gmail.com>
2024-11-16 03:50:22 +0000

    commit 11/15

--- src/your_solution/src/spin_slow_update.cpp
+++ src/your_solution/src/spin_slow_update.cpp
@@ -8,22 +8,23 @@ int main(int argc, char *argv[]) {
 }
 
 SlowSolution::SlowSolution() : Node("slowsolution") {
-  pos_subscription_ = this->create_subscription<ArrayMsg>(
+  pos_subscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
     "measuredpos", 10, std::bind(&SlowSolution::pos_callback, this, std::placeholders::_1));
-  
-  vel_subscription_ = this->create_subscription<ArrayMsg>(
+  /*
+  vel_subscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
     "measuredvel", 10, std::bind(&SlowSolution::vel_callback, this, std::placeholders::_2));
-  publisher_ = this->create_publisher<ArrayMsg::SharedPtr::float>("predictedpos", 10);
+  publisher_ = this->create_publisher<ArrayMsg::SharedPtr::Float32>("predictedpos", 10);
   timer_ = this->create_wall_timer(
   500ms, std::bind(&SlowSolution::timer_callback, this));
   
-  
+  */
+  void SlowSolution::pos_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg) {
+    RCLCPP_INFO(this->get_logger(), "Received position: [%f, %f]", msg->data[0], msg->data[1]);
+  }
 }
 
-void SlowSolution::pos_callback(const ArrayMsg::SharedPtr msg) {
-  RCLCPP_INFO(this->get_logger(), "Received position: [%f, %f]", msg->data[0], msg->data[1]);
-}
 
-void SlowSolution::vel_callback(const ArrayMsg::SharedPtr msg) {
+
+/*void SlowSolution::vel_callback(const ArrayMsg::SharedPtr msg) {
   RCLCPP_INFO(this->get_logger(), "Received velocity: [%f, %f]", msg->data[0], msg->data[1]);
-}
+} */
--- src/your_solution/src/spin_slow_update.h
+++ src/your_solution/src/spin_slow_update.h
@@ -9,20 +9,14 @@ class SlowSolution : public rclcpp::Node {
  public:
   SlowSolution();
  private:
-  void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    }
-  void pos_callback(const ArrayMsg::SharedPtr msg);
-  void vel_callback(const ArrayMsg::SharedPtr msg);
-  rclcpp::Subscription<ArrayMsg>::SharedPtr pos_subscription_;
-  rclcpp::Subscription<ArrayMsg>::SharedPtr vel_subscription_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  size_t count_;
+  //void topic_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg);
+  rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr pos_subscription_;
+ //rclcpp::Subscription<ArrayMsg>::SharedPtr vel_subscription_;
+  //rclcpp::TimerBase::SharedPtr timer_;
+  //rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
+  //size_t count_;
+  //void pos_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg);
+
   
 };
 #endif //YOUR_SOLUTION_SRC_SPIN_SLOW_UPDATE_H_
